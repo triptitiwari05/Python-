b6950a9fe86618b9b41f5d2c8a0e9029752d08a0
TRIPTI TIWARI <triptiaruntiwari797@gmail.com>
2026-01-06 17:06:31 +0530

    learned input and output operation in python

---
+++ basic /input_output.py
@@ -0,0 +1,9 @@
+name = input("name: ")
+age = int(input("age: "))
+height = float(input("height: "))
+print("my name is",name, "i am",age, "years old", "my height is",height, "inches")
+
+print("survey form")
+username = input("username: ")
+password = input("password: ")
+print("my usernsme is",username, "and my password is",password)
